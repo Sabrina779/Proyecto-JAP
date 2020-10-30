23b20655582b758d9d6971e424cd270188e763d1
Sabrina <68930447+Sabrina779@users.noreply.github.com>
2020-10-30 10:07:50 -0300

    Cambio

--- js/cart.js
+++ js/cart.js
@@ -1,70 +1,111 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+var subtotal = 0
+var unidades = 0
+var valorProducto = 0
+var porcentaje = 0;
+var subtotalEnPantalla = false
+var cartProductsArray = [];
 document.addEventListener("DOMContentLoaded", function(e) {
 
     getJSONData(CART_PRODUCTS).then(function(resultObj) {
         if (resultObj.status === "ok") {
             cartProductsArray = resultObj.data.articles
             showCartProducts(cartProductsArray);
-            document.getElementById("buttonTitle").innerHTML = "Seleccione el tipo de envío"
+            document.getElementById("buttonTitle").innerHTML = "Seleccionar tipo de envío"
         }
 
     });
 
-    var subtotal = 0
-    var unidades = 0
-    var valorProducto = 0
-    var porcentaje = 0;
+    const boton = document.getElementById("nextButton")
+    const submitBut = document.getElementById("submitButton")
+    const closebut = document.getElementById("closeButton");
+
+    boton.onclick = function(e) {
+        document.getElementById("FirstPageModal").style.display = "none";
+        boton.style.display = "none"
+        if (document.getElementById("radioCard").checked) {
+            document.getElementById("cardPayment").style.display = "block";
+            submitBut.setAttribute("form", "form2")
+            submitBut.style.display = "block"
+        }
+        if (document.getElementById("radioBank").checked) {
+            document.getElementById("bankPayment").style.display = "block";
+            closebut.style.display = "block"
+        }
+    }
+
+    function deletemodalcontent() {
+        document.getElementById("cardPayment").style.display = "none";
+        document.getElementById("bankPayment").style.display = "none";
+        document.getElementById("FirstPageModal").style.display = "block";
+        boton.style.display = "block";
+        submitBut.style.display = "none";
+        closebut.style.display = "none";
+    }
+    $('#modalID').on('hidden.bs.modal', function(e) {
+        deletemodalcontent()
+    })
+
+    document.getElementById("envioMdv").onclick = function(e) {
+        subtotalEnPantalla = true;
+        porcentaje = 1, 5
+        calcularCostoDeEnvio(porcentaje)
+        document.getElementById("buttonTitle").innerHTML = "Envío dentro de Montevideo"
+    }
+    document.getElementById("envioInterior").onclick = function(e) {
+        porcentaje = 2
+        subtotalEnPantalla = true;
+        calcularCostoDeEnvio(porcentaje)
+        document.getElementById("buttonTitle").innerHTML = "Envío al interior"
+    }
+    document.getElementById("envioSucursal").onclick = function(e) {
+        subtotalEnPantalla = true;
+        porcentaje = 0
+        calcularCostoDeEnvio(porcentaje)
+        document.getElementById("buttonTitle").innerHTML = "Retiro en sucursal"
+    }
 
     function armarsubtotal(unidades, idsubtotal, cartProduct) {
-        console.log(cartProduct)
         valorProducto = cartProduct.unitCost
         if (cartProduct.currency == "USD") {
             subtotal = unidades * valorProducto * 44
         } else {
             subtotal = unidades * valorProducto
         }
+
         document.getElementById(idsubtotal).innerHTML = subtotal
     }
 
     function calcularCostoDeEnvio(porcentaje) {
-        document.getElementById("totalContainer").innerHTML = `
-        <p>Costo de envío ( <span id="porcentaje"></span>%): $<span id="costoEnvio"></span></p>
-        <p>Total: $<span id="total"></span></p>`
-        let total = document.getElementsByClassName("subtotal");
-        let total2 = 0;
-        for (let i = 0; i < total.length; i++) {
-            const element = total[i];
-            total2 += parseInt(element.textContent, 10)
+        let totalProductos = document.getElementsByClassName("subtotal");
+        let subtotalFinal = 0;
+        for (let i = 0; i < totalProductos.length; i++) {
+            const element = totalProductos[i];
+            subtotalFinal += parseInt(element.textContent, 10);
         }
-        let totalEnvio = (total2 * porcentaje) / 100
-        document.getElementById("total").innerHTML = total2 + totalEnvio;
-        document.getElementById("costoEnvio").innerHTML = totalEnvio;
-        document.getElementById("porcentaje").innerHTML = porcentaje
-    }
-
-    document.getElementById("envioMvd").onclick = function(e) {
-        porcentaje = 3.5
-        calcularCostoDeEnvio(porcentaje)
-        document.getElementById("buttonTitle").innerHTML = "Has seleccionado envio dentro de Montevido"
-    }
-    document.getElementById("envioInterior").onclick = function(e) {
-        porcentaje = 5.5
-        calcularCostoDeEnvio(porcentaje)
-        document.getElementById("buttonTitle").innerHTML = "Has seleccionado envio al interior"
-    }
-    document.getElementById("retiroLocal").onclick = function(e) {
-        porcentaje = 0
-        calcularCostoDeEnvio(porcentaje)
-        document.getElementById("buttonTitle").innerHTML = "Has seleccionado retiro en el local"
+        let totalEnvio = (subtotalFinal * porcentaje) / 100
+        if (subtotalEnPantalla) {
+            document.getElementById("totalContainer").style.display = "block";
+            document.getElementById("total").innerHTML = subtotalFinal + totalEnvio;
+            document.getElementById("costoEnvio").innerHTML = totalEnvio;
+            document.getElementById("porcentaje").innerHTML = porcentaje;
+            document.getElementById("subtotalFinal").innerHTML = subtotalFinal;
+        }
     }
 
     function showCartProducts(array) {
         let htmlContentToAppend = "";
-        for (let i = 0; i < array.length; i++) {
-            let product = array[i];
-            htmlContentToAppend += `
+        if (array.length == 0) {
+            document.getElementById("cartProducts").innerHTML = "No hay productos en el carrito";
+            document.getElementById("dropdownMenuButtonShipping").style.display = "none"
+            document.getElementById("totalContainer").style.display = "none";
+
+        } else {
+            for (let i = 0; i < array.length; i++) {
+                let product = array[i];
+                htmlContentToAppend += `
             <a  class="list-group-item list-group-item-action" height="150">
                 <div class="row">
                     <div class="col-3">
@@ -77,33 +118,50 @@ document.addEventListener("DOMContentLoaded", function(e) {
                     </div>
                     <div class="col">
                         <p>Costo unitario: ` + product.currency + "   " + product.unitCost + ` </p>
-                        <label for="quantity">Cantidad:</label>
-                        <input type="number" value="${product.count}" class="quantity"  min="1" max="5">
+                    <label for="quantity">Cantidad:</label>
+                    <input type="number" value="${product.count}" class="quantity"  min="1" max="5">
+                    </div>
+                    <div class="col-auto">
+                    <button name="borrar" id="${i}" type="button" class="close" d aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                     </div>
                 </div>
                 <div class="row justify-content-end">
                     <div class="col-4">
-                    <p class="text-right" style="color:red">Subtotal: $  <span class="subtotal" id="subtotal` + i + `"></span></p> 
+                    <p class="text-right" style="color:#ff9e8f">Subtotal del producto: $  <span class="subtotal" id="subtotal` + i + `"></span></p> 
                     </div>
                 </div>
             </a>
             `
-            document.getElementById("cartProducts").innerHTML = htmlContentToAppend;
-
+                document.getElementById("cartProducts").innerHTML = htmlContentToAppend;
+            }
         }
+
         for (let i = 0; i < array.length; i++) {
             let product = array[i];
             armarsubtotal(product.count, "subtotal" + i, product);
+            calcularCostoDeEnvio(porcentaje)
+
         }
 
         let cantidades = document.getElementsByClassName("quantity");
-        console.log(cantidades)
         for (let i = 0; i < cantidades.length; i++) {
             let product = array[i];
             const element = cantidades[i];
             element.onchange = function(e) {
+                product.count = e.target.value
+
                 armarsubtotal(e.target.value, "subtotal" + i, product)
                 calcularCostoDeEnvio(porcentaje)
+            }
+        }
+        let buttonserase = document.getElementsByName("borrar")
+        for (let i = 0; i < buttonserase.length; i++) {
+            const boton = buttonserase[i];
+            boton.onclick = function(e) {
+                cartProductsArray.splice(boton.id, 1)
+                showCartProducts(cartProductsArray)
             }
         }
     }
